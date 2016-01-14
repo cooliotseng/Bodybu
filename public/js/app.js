4e29dfc9dba3ff2d89fc2c73d9238395c641c5a7
root <root@819a6a2d36e1.(none)>
2016-01-14 11:15:50 +0000

    build env

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+import "bootstrap/css/bootstrap.css!";
+import $ from "jquery"
+import _ from "underscore"
+import Backbone from "backbone"
+
+
+
+/**
+* SETUP
+**/
+var app = app || {};
+
+
+/**
+* MODELS
+**/
+app.Message = Backbone.Model.extend({
+    url: function() {
+      
+    },
+    id: null,
+    defaults: {
+      
+    }
+});
+
+/**
+* VIEWS
+**/
+app.ContentView = Backbone.View.extend({
+    // constructor
+    initialize: function() {
+      
+    },
+    render: function() {
+       
+    },
+    read: function(e) {
+       
+    }
+});
+
+
+$(document).ready(function(){
+   
+});
